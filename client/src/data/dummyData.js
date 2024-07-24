8bbb7d3bfe3d56ecaad0fd6d372b9792f181830b
THIYAGHEESWARI G <thiyagheeswarig@gmail.com>
2024-07-24 09:24:24 +0530

    Create dummyData.js

---
+++ client/src/data/dummyData.js
@@ -0,0 +1,11 @@
+export const dummyData = [
+    {
+      id: 1,
+      url: 'http://example.com/image1.png',
+    },
+    {
+      id: 2,
+      url: 'http://example.com/image2.png',
+    },
+  ];
+  
